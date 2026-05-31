34fc227fc3aa05e585c54fb00954e7ab859ce109
Benjamin Wagner <benjaminwagner@Benjamins-Mac-Studio.local>
2026-05-31 01:39:13 -0500

    Return background to pre-smoke version

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,5 @@
 import '../styles/globals.css'
-import { useEffect, useRef } from 'react'
-
-function SmokeBackground() {
-  const videoRef = useRef(null)
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.32
-    }
-  }, [])
-
-  return (
-    <div className="smoke-video-bg" aria-hidden="true">
-      <video ref={videoRef} autoPlay muted loop playsInline preload="auto">
-        <source src="/smoke-vertical-purple.mp4" type="video/mp4" />
-      </video>
-    </div>
-  )
-}
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <SmokeBackground />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
